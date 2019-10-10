e211bc89623387e4b13881be53ba98560eddcea8
aluno <you@example.com>
2019-10-10 12:22:24 -0300

    segundo commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const routers = require('./routes/');
+const routers = require('./node_modules');
 
 const app = express();
 app.use(bodyParser.jason());
